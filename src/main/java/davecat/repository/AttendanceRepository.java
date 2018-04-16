8d5d2e8c65df2a859b9e713b847452c9f94fed58
David <davidka1997@gmail.com>
2018-04-16 03:40:52 +0200

    AttendanceRepositry annotated as Repository

--- src/main/java/davecat/repository/AttendanceRepository.java
+++ src/main/java/davecat/repository/AttendanceRepository.java
@@ -2,9 +2,11 @@ package davecat.repository;
 
 import davecat.modell.Attendance;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface AttendanceRepository extends CrudRepository<Attendance, UUID> {
 
 }
