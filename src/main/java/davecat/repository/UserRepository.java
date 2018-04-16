cd9b7e5dfd73d92af562863ec6443abcdcd1379a
David <davidka1997@gmail.com>
2018-04-16 03:41:46 +0200

    UserRepositry annotated as Repository

--- src/main/java/davecat/repository/UserRepository.java
+++ src/main/java/davecat/repository/UserRepository.java
@@ -2,9 +2,11 @@ package davecat.repository;
 
 import davecat.modell.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, UUID> {
 
 }
